46c270b4fb7fb6d8ed7845f3ea14feeb3e5d26ab
unknown <davidstew87@gmail.com>
2022-11-17 11:47:39 -0500

    set up folder structure, and got logic for the tests to work

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can set GitHUb account via constructor", () => {
+  //code goes here
+  const github = "github";
+  const e = new Engineer("jane",1,"test@test.com",github);
+  expect(e.github).toBe(github);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+  //code goes here
+  const testValue = "Engineer";
+  const e = new Engineer("Jane", 1, "test@test.com", "github");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+  //code goes here
+  const testValue = "github";
+  const e = new Engineer("Jane",1,"test@test.com",testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
